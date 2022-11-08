87de3a1c37cd3dc9587ff04a37501036be0ea1e2
AdnanOlarmmi <olarmmi@gmail.com>
2022-11-08 13:25:21 +0100

    Fix Linters Error

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,13 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-const reducers = combineReducers({
-  book: booksReducer,
-  categories: categoriesReducer,
-});
-
 const store = configureStore({
-    reducer: reducers,
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
 });
 
 export default store;
