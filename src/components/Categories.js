c1194601b62cb241ec102506f11e0b5b7689fe91
AdnanOlarmmi <olarmmi@gmail.com>
2022-11-09 12:17:44 +0100

    Fix linters error

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,20 +3,20 @@ import { useDispatch, useSelector } from 'react-redux';
 import { checkCategory } from '../redux/categories/categories';
 
 const Categories = () => {
-  const categories = useSelector(state=>state.categories);
+  const categories = useSelector((state) => state.categories);
 
   const dispatch = useDispatch();
   const onCheck = (e) => {
     e.preventDefault();
-    dispatch(checkCategory())
-  }
+    dispatch(checkCategory());
+  };
 
   return (
     <div>
-  <button type="submit" onClick={onCheck}>Check Status</button>
-    <p>{categories}</p>
+      <button type="submit" onClick={onCheck}>Check Status</button>
+      <p>{categories}</p>
     </div>
-    )
+  );
 };
 
 export default Categories;
