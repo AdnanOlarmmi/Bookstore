3708d268dc5c8c3323546fc58b0f962dbbdaaee6
AdnanOlarmmi <olarmmi@gmail.com>
2022-11-09 12:16:46 +0100

    Add click event to category check status

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkCategory } from '../redux/categories/categories';
 
-const Categories = () => (
-  <button type="submit">Check Status</button>);
+const Categories = () => {
+  const categories = useSelector(state=>state.categories);
+
+  const dispatch = useDispatch();
+  const onCheck = (e) => {
+    e.preventDefault();
+    dispatch(checkCategory())
+  }
+
+  return (
+    <div>
+  <button type="submit" onClick={onCheck}>Check Status</button>
+    <p>{categories}</p>
+    </div>
+    )
+};
 
 export default Categories;
